97aba54cfae2a606af1325396f456f5e0754f3d7
sarthak7205 <14as1913075@gmail.com>
2023-12-25 15:50:35 +0530

    git

---
+++ task 1/index.js
@@ -0,0 +1,11 @@
+const header =document.getElementById('header');
+header.style.backgroundColor='green';
+header.style.color='orange';
+header.style.borderBottom="3px solid orange";
+const h=document.getElementById('main-heading');
+h.textContent='Fruit World';
+const bh=document.getElementById('basket-heading');
+bh.style.color='green';
+const thank=document.getElementById('thanks');
+thank.innerHTML='<p>Please visit us again</p>';
+thank.style.fontWeight='bold';
